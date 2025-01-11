4497cf45e7bd2039b260d611093ce83ff78257da
Md. Rahat <rahat.cse5.bu@gmail.com>
2025-01-11 12:57:34 +0600

    bottom nav more item bottom sheet done

--- lib/app/modules/home/view/home_view.dart
+++ lib/app/modules/home/view/home_view.dart
@@ -4,6 +4,7 @@ import 'package:prostuti/app/modules/home/widgets/ranking_view.dart';
 import '../../../common/custom_appbar.dart';
 import '../../../common/widgets/bottom_nav_bar_widget.dart';
 import '../controller/home_controller.dart';
+import '../widgets/home_bottom_nav_more_bottom_sheet.dart';
 import '../widgets/home_main_widget.dart';
 
 class HomeView extends GetView<HomeController> {
@@ -25,12 +26,24 @@ class HomeView extends GetView<HomeController> {
           // leadingWidth: 100,
 
           name: "Rahat"),
-      body: Obx(() =>
-          _pages[controller.currentIndex.value]),
+      body: Obx(() => _pages[controller.currentIndex.value]),
       bottomNavigationBar: Obx(() => CustomBottomNavBar(
             currentIndex: controller.currentIndex.value,
             onTap: (value) {
-              controller.currentIndex.value = value;
+              if (value == 4) {
+                Get.bottomSheet(
+                  HomeBottomNavMoreBottomSheet(),
+                  isScrollControlled:
+                      false, // Optional, to allow for full-screen or scrollable content
+                  backgroundColor: Colors
+                      .white, // Optional, background color for the bottom sheet
+                      ignoreSafeArea: false, 
+                   isDismissible: true, 
+
+                );
+              } else {
+                controller.currentIndex.value = value;
+              }
             },
           )),
     );
