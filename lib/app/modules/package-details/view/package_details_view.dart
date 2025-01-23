b8acefaeead15b6b1827833783c8a614abe03605
Md. Rahat <rahat.cse5.bu@gmail.com>
2025-01-23 08:14:23 +0600

    package details initiated

---
+++ lib/app/modules/package-details/view/package_details_view.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../controllers/package_details_controller.dart';
+
+class PackageDetailsView extends GetView<PackageDetailsController> {
+  const PackageDetailsView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
